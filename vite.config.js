2acd379ddb457e3be9aadaaa2e759fd45c5f036f
Saint Johane Divin Ngalessami Eba <st.ngalessami@gmail.com>
2026-09-22 22:07:24 +0100

    chore: build production (chunk split three/cannon)

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: { port: 5173, host: true },
-  build: { target: 'es2020', sourcemap: false },
+  build: {
+    target: 'es2020',
+    sourcemap: false,
+    cssCodeSplit: true,
+    chunkSizeWarningLimit: 900,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+          physics: ['cannon-es'],
+        },
+      },
+    },
+  },
 })
